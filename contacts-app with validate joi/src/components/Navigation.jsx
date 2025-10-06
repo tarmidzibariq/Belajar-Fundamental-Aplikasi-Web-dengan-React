c71318838564a3083dc21271d6e4dbc45a39e1e1
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-10-06 15:33:04 +0700

    implement authentication with login and registration features

--- contacts-app with validate joi/src/components/Navigation.jsx
+++ contacts-app with validate joi/src/components/Navigation.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FiHome, FiPlusCircle } from 'react-icons/fi';
-function Navigation() {
+import {Link} from "react-router-dom";
+import {FiHome, FiPlusCircle, FiLogOut} from 'react-icons/fi';
+import PropTypes from "prop-types";
+
+function Navigation({logout, name}) {
     return (
         <nav className="navigation">
             <ul>
-                <li><Link to='/'><FiHome /></Link> </li>
-                <li><Link to="/add"><FiPlusCircle /></Link></li>
+                <li>
+                    <Link to='/'><FiHome/></Link>
+                </li>
+                <li>
+                    <Link to="/add"><FiPlusCircle/></Link>
+                </li>
+                <li>
+                    <button onClick={logout}>{name} <FiLogOut/></button>
+                </li>
             </ul>
-
         </nav>
     );
 }
+
+Navigation.propTypes = {
+    logout: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired
+};
 
 export default Navigation;
---
+++ contacts-app with validate joi/src/pages/RegisterPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Link, Navigate} from 'react-router-dom';
+import RegisterInput from '../components/RegisterInput';
+import {register} from '../utils/api';
+import {useNavigate} from 'react-router-dom';
+
+function RegisterPage() {
+    
+    const navigate = useNavigate();
+
+    async function onRegisterHandler(user) {
+        
+        const {error} = await register(user);
+        if (!error) {
+            navigate('/');
+        }
+    }
+
+    return (
+        <section className='register-page'>
+            <h2>Gak perlu serius-serius ya isinya ...</h2>
+            <RegisterInput register={onRegisterHandler}/>
+            <p>Kembali ke
+                <Link to="/">Masuk</Link>
+            </p>
+        </section>
+    )
+}
+
+export default RegisterPage;
